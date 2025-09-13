6e833d710524f50d2ae5df8144ce13734ea6bf8f
Barnil <barnil2003>
2025-09-14 04:03:43 +0530

    add search bar

--- src/pages/Jobs/Jobs.jsx
+++ src/pages/Jobs/Jobs.jsx
@@ -8,6 +8,7 @@ const Jobs=()=>{
                 <div className={styles.filterContainer}>
                     <span>Filter by:</span>
                     <select className={styles.filter}>
+                        <option value="all" className={styles.option}>All</option>
                         <option value="type" className={styles.option}>Job Type</option>
                         <option value="mode" className={styles.option}>Job Mode</option>
                         <option value="exp" className={styles.option}>Experience</option>
