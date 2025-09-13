855b6776f16629443b4a387fbf16203aa90c2f74
Barnil <barnil2003>
2025-09-14 04:47:14 +0530

    add some styles in the card

--- src/components/Jobs/Card.jsx
+++ src/components/Jobs/Card.jsx
@@ -1,9 +1,11 @@
+import styles from "./Card.module.scss";
+
 const Card=(props)=>{
     return (
-        <div>
-            <h1>{props.name}</h1>
-            <h3>{props.mode}|{props.type}</h3>
-            <h3>{props.exp} Experience Required</h3>
+        <div className={styles.Card}>
+            <h1 className={styles.h1}>{props.name}</h1>
+            <h3 className={styles.h2}>{props.mode}|{props.type}</h3>
+            <h3 className={styles.h3}>{props.exp} Experience Required</h3>
         </div>
     );
 }
